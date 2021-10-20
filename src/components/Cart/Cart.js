ad3a72b656d3be4745ab32700cb366b4ea65340c
Jethro Williams <jethrowilliams@outlook.com>
2021-10-20 14:41:58 +0100

    Add CartItem component, and use 'bind' to point at functions with arguments, without immediately calling the functions

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,55 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
+import { useContext } from "react";
+
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
+import classes from "./Cart.module.css";
 
 const Cart = (props) => {
-  const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}</ul>;
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+          // 'bind' points at a function for future execution, and allows you to pass in the arguments for when it's executed
+          // Basically, it allows you to point at the 'cartItemAddHandler', with arguments, without calling it
+          // The first argument of 'bind' sets what 'this' would be within the function. As we don't need to use 'this', we can pass the first argument as 'null'
+          // Subsequent arguments are the parameters that we pass into the function
+          // As used here, it is equivalent to 'onAdd={() => cartItemAddHandler(item)}'
+        />
+      ))}
+    </ul>
+  );
 
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
-  )
+  );
 };
 
 export default Cart;
