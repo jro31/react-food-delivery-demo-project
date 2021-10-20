acf4d307ab0becaa39fc13b4abf7576e37bd4a01
Jethro Williams <jethrowilliams@outlook.com>
2021-10-20 12:53:45 +0100

    Add CartContent, CartProvider and use with useContext(), and add function to get total from one value of array of objects

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,21 @@
+import { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+  const cartCtx = useContext(CartContext);
+  // The CartContext is managed by the 'closest' provider (in this case, that's the 'CartProvider' in app.js (it's the only provider))
+  // The 'HeaderCartButton' will be re-evaluated whenever the CartContext changes
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+  // Loops over all 'cartCtx.items', and on each iteration returns 'currentNumber + item.amount'
+  // The starting 'currentNumber' number is 0 (the second argument)
+  // On each subsequent iteration, 'currentNumber' will be the return from the previous loop
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
@@ -11,7 +25,7 @@ const HeaderCartButton = props => {
         Your cart
       </span>
       <span className={classes.badge}>
-        3
+        {numberOfCartItems}
       </span>
     </button>
   )
