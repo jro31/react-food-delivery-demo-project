0a3563269c6a174f4a53e06f622020f2ea672a75
Jethro Williams <jethrowilliams@outlook.com>
2021-10-20 13:53:23 +0100

    Add a reducer (cartReducer) to handle the cart, and use refs (in a custom component) to pass the entered value to the reducer

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,14 @@
+import React from 'react';
+
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id} >{props.label}</label>
-      <input {...props.input} /> {/* The spread operator here means that all key/value pairs of 'props.input' get added as props to <input>. For example, if 'props.input' is {id: 'm1', type: 'text', placeholder: 'Monkey'}, these attributes would be added to the <input> tag */}
+      <input ref={ref} {...props.input} /> {/* The spread operator here means that all key/value pairs of 'props.input' get added as props to <input>. For example, if 'props.input' is {id: 'm1', type: 'text', placeholder: 'Monkey'}, these attributes would be added to the <input> tag */}
     </div>
   )
-};
+});
 
 export default Input;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,56 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
-const CartProvider = props => {
-  const addItemToCartHandler = item => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+}
 
-  const removeItemfromCartHandler = id => {};
+const cartReducer = (state, action) => {
+  if(action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    // We use 'concat()' instead of 'push()', because concat returns a new array
+    // 'push()' updates the existing array, which we don't want to do
+    // You don't want to edit your old state snapshot, because it would get edited in memory without React knowing about it
+    // Instead you want to create a brand new state object, and return that
+
+    const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return defaultCartState
+};
+// 'cartReducer' is outside of the component function because it won't need anything from the component
+// and because it shouldn't be recreated every time the component is re-evaluated
+
+const CartProvider = props => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+  // The first array element returned by useReducer is your state snapshot ('cartState' here)
+  // The second is the function that allows you to dispatch an action to the reducer ('dispatchCartAction')
+  // The first argument of useReducer() points at your reducer function
+  // The second argument is the initial state
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({
+      type: 'ADD',
+      item: item,
+    });
+  };
+
+  const removeItemfromCartHandler = (id) => {
+    dispatchCartAction({
+      type: 'REMOVE',
+      id: id,
+    });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemfromCartHandler,
   };
